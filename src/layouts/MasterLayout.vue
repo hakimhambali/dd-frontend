09da29519220b82e229e399759fa053d9c50c018
Alauddin <alauddin3604@gmail.com>
2024-04-04 16:49:28 +0800

    Wrap with `page` class

--- src/layouts/MasterLayout.vue
+++ src/layouts/MasterLayout.vue
@@ -17,31 +17,33 @@ onMounted(() => {
 </script>
 
 <template>
-    <TopNavBar />
-    <div class="page-wrapper">
-        <div class="container" v-if="$route.meta.title">
-            <div class="page-header d-print-none">
-                <div class="row g-2 align-items-center">
-                    <div class="col">
-                        <!-- <a href="#" @click.prevent="$router.go(-1)" role="button" class="mb-2 d-inline-block"
+    <div class="page">
+        <TopNavBar />
+        <div class="page-wrapper">
+            <div class="container" v-if="$route.meta.title">
+                <div class="page-header d-print-none">
+                    <div class="row g-2 align-items-center">
+                        <div class="col">
+                            <!-- <a href="#" @click.prevent="$router.go(-1)" role="button" class="mb-2 d-inline-block"
                             v-if="showBackButton($route.meta.title)">
                             <icon name="chevron-left" />
                             <span>Back</span>
                         </a> -->
-                        <h2 class="page-title">
-                            {{ $route.meta.title }}
-                        </h2>
+                            <h2 class="page-title">
+                                {{ $route.meta.title }}
+                            </h2>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="page-body">
-            <div class="container" id="page-container">
-                <router-view></router-view>
+            <div class="page-body">
+                <div class="container" id="page-container">
+                    <router-view></router-view>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="toast-container position-fixed top-0 end-0 p-3">
-        <BaseToast v-for="toast in toasts" :key="toast.id" :toast="toast" />
+        <div class="toast-container position-fixed top-0 end-0 p-3">
+            <BaseToast v-for="toast in toasts" :key="toast.id" :toast="toast" />
+        </div>
     </div>
 </template>
