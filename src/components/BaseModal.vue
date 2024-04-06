d6c2c1a1b66ec6f558c0b10a89d4c70ef50fd37a
Alauddin <alauddin3604@gmail.com>
2024-04-07 00:00:37 +0800

    Refactor

--- src/components/BaseModal.vue
+++ src/components/BaseModal.vue
@@ -2,23 +2,25 @@
 import { computed } from 'vue'
 
 const props = withDefaults(defineProps<{
+    fade?: boolean
     modalId: string
     modalSize?: string
     modalBackdrop?: string | boolean
     modalKeyboard?: boolean
     verticallyCentered?: boolean
 }>(), {
+    fade: true,
     modalSize: 'sm',
     modalBackdrop: true,
     modalKeyboard: true,
     verticallyCentered: true
 })
 
-const verticallyCenteredClass = computed(() => props.verticallyCentered ? 'modal-dialog-centered ' : null)
+const verticallyCenteredClass = computed(() => props.verticallyCentered ? 'modal-dialog-centered ' : '')
 </script>
 
 <template>
-    <div class="modal fade" role="dialog" tabindex="-1" :id="modalId" :data-bs-backdrop="modalBackdrop"
+    <div class="modal" :class="{ fade }" role="dialog" tabindex="-1" :id="modalId" :data-bs-backdrop="modalBackdrop"
         :data-bs-keyboard="modalKeyboard">
         <div :class="`modal-dialog modal-${modalSize} ${verticallyCenteredClass}`" role="document">
             <div class="modal-content">
