7877fc7739837d975ad845d99be15f16e0046c66
Alauddin <alauddin3604@gmail.com>
2024-04-07 00:01:57 +0800

    Init dropdown in navbar

--- src/components/TopNavBar.vue
+++ src/components/TopNavBar.vue
@@ -1,14 +1,12 @@
 <script setup lang="ts">
 import AuthService from '@/services/AuthService'
 import useIsAuthStore from '@/stores/auth'
-import useUserStore from '@/stores/user'
 import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 
 const route = useRoute()
 const router = useRouter()
 const { revokeUser } = useIsAuthStore()
-const userStore = useUserStore()
 
 const links = computed(() => {
     const links = [
@@ -46,7 +44,7 @@ const logout = async (): Promise<void> => {
 </script>
 
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light d-print-none">
+    <nav class="navbar navbar-expand-lg navbar-light d-print-none px-5">
         <div class="container-fluid">
             <router-link :to="{ name: 'home-page' }" class="navbar-brand fw-bold me-5">Home</router-link>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
@@ -63,12 +61,32 @@ const logout = async (): Promise<void> => {
                             {{ link.text }}
                         </router-link>
                     </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                            aria-expanded="false">Settings</a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
                 </ul>
             </div>
-            <div class="d-flex align-items-center">
-                <span class="me-3">Hi, {{ userStore.name }}!</span>
-                <button class="btn btn-dark" @click.prevent="logout">Logout</button>
+            <div class="navbar-nav flex-row order-md-last">
+                <div class="nav-item dropdown">
+                    <a href="#" class="nav-link d-flex lh-1 text-reset p-0" data-bs-toggle="dropdown">
+                        <span class="avatar avatar-sm" style="background-image: url(...)"></span>
+                        <div class="d-none d-xl-block ps-2">
+                            <div>Alauddin</div>
+                            <div class="mt-1 small text-secondary">Full Stack Developer</div>
+                        </div>
+                    </a>
+                    <ul class="dropdown-menu dropdown-menu-end">
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><a class="dropdown-item" href="#" @click.prevent="logout">Logout</a></li>
+                    </ul>
+                </div>
             </div>
         </div>
     </nav>
-</template>@/stores/auth
+</template>
